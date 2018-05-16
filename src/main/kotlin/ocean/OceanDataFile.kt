ca49093458b3aeb25670767603b38078e2b295f3
Sébastien Augsburger <sebastien.augsburger.ext@snef.fr>
2018-05-16 14:21:34 +0200

    Updated OCEAN_ANTENNE data file to correspond to new table structure

--- src/main/kotlin/ocean/OceanDataFile.kt
+++ src/main/kotlin/ocean/OceanDataFile.kt
@@ -1,6 +1,7 @@
 package ocean
 
 import DataFile
+import java.sql.Timestamp
 
 abstract class OceanDataFile : DataFile() {
     override val fileCharset = DataFile.CHARSET_ANSI
@@ -10,4 +11,12 @@ abstract class OceanDataFile : DataFile() {
     override val tableHeader get() = fileHeader.enumConstants.map { it.name }
 
     override val quoteChar = '"'
+
+    protected fun String.toTimestamp(): Timestamp = Timestamp.valueOf(this)
+
+    protected fun String.toTimestampOrNull() = try {
+        toTimestamp()
+    } catch (ex: IllegalArgumentException) {
+        null
+    }
 }
