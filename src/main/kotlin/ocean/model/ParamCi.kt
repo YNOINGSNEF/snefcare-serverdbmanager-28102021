512758fa08d485d83715958c9ce1b8bfd7ddfc47
Sébastien Augsburger <sebastien.augsburger.ext@snef.fr>
2018-05-18 14:00:39 +0200

    Refactor : simplified OceanDataFile implementation

--- src/main/kotlin/ocean/model/ParamCi.kt
+++ src/main/kotlin/ocean/model/ParamCi.kt
@@ -3,34 +3,17 @@ package ocean.model
 import ocean.OceanDataFile
 import org.apache.commons.csv.CSVRecord
 import java.sql.PreparedStatement
-import java.text.ParseException
 
 class ParamCi : OceanDataFile() {
     override val fileName = "OCEAN_PARAMCI"
     override val fileHeader = Header::class.java
 
-    override fun addBatch(stmt: PreparedStatement, record: CSVRecord): Boolean {
+    override fun populateStatement(stmt: PreparedStatement, record: CSVRecord) {
         var index = 0
-        return try {
-            stmt.setString(++index, record[Header.PCI_NOM])
-            stmt.setInt(++index, record[Header.PCI_VAL].toInt())
-            stmt.setInt(++index, record[Header.PCI_TYPE].toInt())
-            stmt.addBatch()
-            true
-        } catch (ex: NumberFormatException) {
-            stmt.clearParameters()
-            false
-        } catch (ex: ParseException) {
-            stmt.clearParameters()
-            false
-        } catch (ex: IllegalArgumentException) {
-            stmt.clearParameters()
-            false
-        } catch (ex: Exception) {
-            println("        > An error occurred : ${ex.message}")
-            stmt.clearParameters()
-            false
-        }
+
+        stmt.setString(++index, record[Header.PCI_NOM])
+        stmt.setInt(++index, record[Header.PCI_VAL].toInt())
+        stmt.setInt(++index, record[Header.PCI_TYPE].toInt())
     }
 
     enum class Header {
