4073b88520a16d7835be43f64ea5cc10452a7770
Sébastien Augsburger <sebastien.augsburger@amaris.com>
2021-07-23 14:25:51 +0200

    Fix fix duplicated antennas

--- src/main/kotlin/fr/snef/dbmanager/orange/procedures/ProcedureAntennas.kt
+++ src/main/kotlin/fr/snef/dbmanager/orange/procedures/ProcedureAntennas.kt
@@ -6,7 +6,7 @@ object ProcedureAntennas : OrangeProcedureDataFile() {
     override val tableName = "ANTENNA"
     override val procedureQuery = """
         INSERT INTO ANTENNA(id_orf, sector_number, azimuth, reference, manufacturer, is_installed, hba, site_id)
-        SELECT MAX(E.EQPT_ID), C.SECTEUR, C.AZM_SYNOP, E.EQPT_CATALOG, E.MANUFACTURER, NOT(E.IS_PREV) AS IS_INSTALLED, IFNULL(C.HBA, 0) AS A_HBA, IDS.SITE_ID
+        SELECT MAX(E.EQPT_ID), C.SECTEUR, C.AZM_SYNOP, E.EQPT_CATALOG, E.MANUFACTURER, MAX(NOT(E.IS_PREV)) AS IS_INSTALLED, IFNULL(C.HBA, 0) AS A_HBA, IDS.SITE_ID
         FROM (
             SELECT ID, SITE_ID FROM CELL_2G UNION
             SELECT ID, SITE_ID FROM CELL_3G UNION
@@ -16,6 +16,6 @@ object ProcedureAntennas : OrangeProcedureDataFile() {
         JOIN TMP_CELL C ON C.ID = IDS.ID
         JOIN TMP_EQUIPMENT E ON E.CELLULAR_NODE_ID = C.ID_ORF
         WHERE $equipmentAntennaFilter
-        GROUP BY C.SECTEUR, C.AZM_SYNOP, E.EQPT_CATALOG, E.MANUFACTURER, IS_INSTALLED, A_HBA, IDS.SITE_ID;
+        GROUP BY C.SECTEUR, C.AZM_SYNOP, E.EQPT_CATALOG, E.MANUFACTURER, A_HBA, IDS.SITE_ID;
     """
 }
